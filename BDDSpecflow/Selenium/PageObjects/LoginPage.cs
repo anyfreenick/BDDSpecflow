111a88b1d776744308bef3f9e7fe3ad1029cb920
Maksim Titov <maksim_titov@epam.com>
2016-09-16 16:16:44 +0300

    page object for login page

---
+++ BDDSpecflow/Selenium/PageObjects/LoginPage.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDSpecflow.Selenium.PageObjects
+{
+    public class LoginPage
+    {
+        private const string _url = "https://signin.ebay.com/";
+        private IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement LoginField
+        {
+            get { return _driver.FindElement(By.XPath("(//div[@id='pri_signin']//input[@class='fld'])[1]")); }
+        }
+
+        public IWebElement PasswordField
+        {
+            get { return _driver.FindElement(By.XPath("(//div[@id='pri_signin']//input[@class='fld'])[2]")); }
+        }
+
+        public IWebElement SubmitButton
+        {
+            get { return _driver.FindElement(By.Id("sgnBt")); }
+        }
+
+        public IWebElement LoginErrorMessage
+        {
+            get { return _driver.FindElement(By.Id("errf")); }
+        }
+
+        public string LoginErrorMessageText
+        {
+            get { return LoginErrorMessage.GetAttribute("aria-describedby"); }
+        }
+
+        public void Open()
+        {
+            _driver.Navigate().GoToUrl(_url);
+            _driver.Manage().Window.Maximize();
+        }
+
+        public bool ErrorMessageDisplayed()
+        {
+            if (LoginErrorMessage.Displayed)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ BDDSpecflow/Selenium/WebDriver.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace BDDSpecflow.Selenium
+{
+    public sealed class WebDriver
+    {
+        private static IWebDriver _webdriver;
+
+        private static WebDriver instance;
+
+        private WebDriver() { }
+
+        public static WebDriver GetInstance(DriverType drivertype)
+        {
+            if (instance == null)
+            {
+                instance = new WebDriver();
+                switch (drivertype)
+                {
+                    case DriverType.Firefox:
+                        instance.Driver = new FirefoxDriver();
+                        break;
+                    case DriverType.Chrome:
+                        instance.Driver = new ChromeDriver();
+                        break;
+                    default:
+                        instance.Driver = new FirefoxDriver();
+                        break;
+                }
+            }
+            return instance;
+        }
+
+        public IWebDriver Driver
+        {
+            private set { _webdriver = value; }
+            get { return _webdriver; }
+        }
+    }
+    public enum DriverType
+    {
+        Firefox,
+        Chrome
+    }
+}
