40ab388d30fc64ddf26e0f4994105c0836f2fc16
Maksim Titov <maksim_titov@epam.com>
2016-09-16 16:50:32 +0300

    feature LoginNegative done

---
+++ BDDSpecflow/Specflow/Features/LoginNegative.feature
@@ -0,0 +1,17 @@
+﻿Feature: LoginNegative
+	As a user I would like to be sure that it is not possible to Sing In if login or pssword is empty
+
+@LoginNegative
+Scenario Outline: Sing In with empty field
+
+Given 	Aliexpress Sign In page is open
+When 	I set Account "<account>"
+And     I set Password "<psw>"
+And     I click SignIn button
+Then    I get error "<message>"
+
+Examples:
+|account		       |psw	|message					      |
+|			           |	|К сожалению, данные не совпадают.|
+|shaforost.olya@mail.ru|	|К сожалению, данные не совпадают.|
+|			           |abc	|К сожалению, данные не совпадают.|
---
+++ BDDSpecflow/Specflow/StepDefenitions/LoginNegative.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using BDDSpecflow.Selenium;
+using BDDSpecflow.Selenium.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BDDSpecflow.Specflow.StepDefenitions
+{
+    [Binding]
+    public sealed class LoginNegative
+    {
+
+        static WebDriver driver;
+        LoginPage loginpage;
+
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+        [Given]
+        public void GivenAliexpressSignInPageIsOpen()
+        {
+            driver = WebDriver.GetInstance(DriverType.Chrome);
+            loginpage = new LoginPage(driver.Driver);
+            loginpage.Open();
+        }
+
+        [When(@"I set Account ""(.*)""")]
+        public void WhenISetAccount(string login)
+        {
+            loginpage.LoginField.SendKeys(login);
+        }
+
+        [When(@"I set Password ""(.*)""")]
+        public void WhenISetPassword(string password)
+        {
+            loginpage.PasswordField.SendKeys(password);
+        }
+
+        [When(@"I click SignIn button")]
+        public void WhenIClickSignInButton()
+        {
+            loginpage.SubmitButton.Click();
+        }
+
+        [Then(@"I get error ""(.*)""")]
+        public void ThenIGetError(string errorMessage)
+        {
+            Assert.IsTrue(loginpage.LoginErrorMessageText == errorMessage);
+        }
+
+        [AfterFeature]
+        static public void ThenBrowserIsClosed()
+        {
+            driver.Driver.Quit();
+        }
+    }
+}
