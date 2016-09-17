5dcc3482df5bb278daac32f7f5472658909acbff
Maksim Titov <maksim_titov@epam.com>
2016-09-17 14:46:02 +0300

    1

---
+++ BDDSpecflow/Selenium/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BDDSpecflow.Selenium;
+using BDDSpecflow.Selenium.PageObjects;
+using OpenQA.Selenium;
+
+namespace BDDSpecflow.Selenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WebDriver driver = WebDriver.GetInstance(DriverType.Chrome);
+            MainPage mainpage = new MainPage(driver.Driver);
+            mainpage.Open();
+            mainpage.OpenJewlryParagraph();
+            FineJewelryPage jewelPage = new JewelryAndWatchesPage(driver.Driver).OpenFineJewelryPage();
+            jewelPage.Items[1].Click();
+        }
+    }
+}
